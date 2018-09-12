2b216460eb514fe359e9751dcf1500a260698309
Matthieu Green <matthieu.green@dauphine.eu>
2018-09-12 18:07:16 +0200

    ng build

--- serveur.js
+++ serveur.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 app.use('/', express.static('dist/e-thik'))
 
-app.listen(80, () => console.log('Example app listening on port 80!'))
+app.listen(4000, () => console.log('Example app listening on port 4000!'))
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/my-web-app/index.html'));
