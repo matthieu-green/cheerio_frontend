d855abeff10d77e936d62b131f6fff053070da35
Matthieu Green <matthieu.green@dauphine.eu>
2018-09-12 18:02:41 +0200

    changed path

--- serveur.js
+++ serveur.js
@@ -7,5 +7,5 @@ app.use('/', express.static('dist/e-thik'))
 app.listen(80, () => console.log('Example app listening on port 80!'))
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/e-thik/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/my-web-app/index.html'));
 });
